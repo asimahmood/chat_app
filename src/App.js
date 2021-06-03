bd3d035e27c013fbc1d61d61fbb0fe22b6a0fef4
Asim Mahmood <31977970+asimmahmood88@users.noreply.github.com>
2021-06-03 17:33:59 +0500

    Updated App.js and added new folder
    I added new prop "renderChatFeed" and created component folder.  I added "ChatFeed" file to the component filder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import  Chat Feed  from './components/ChatFeed';
 
 const App = () => {
     return <ChatEngine 
         height="100vh"
-        projectID=""
-        userName=""
-        userSecret=""
+        projectID="c2ba0d03-7dd6-4200-8099-40f95a10c0ab"
+        userName="mecallmathers"
+        userSecret="123123"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />   
 
 }
