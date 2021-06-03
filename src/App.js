12951f6141867ee4a1d676d2805b4384bc83b5f3
Asim Mahmood <31977970+asimmahmood88@users.noreply.github.com>
2021-06-03 18:05:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import  ChatFeed  from './components/ChatFeed';
 
+
 const App = () => {
     return <ChatEngine 
         height="100vh"
