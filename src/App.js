c093b65f55932895d5c841030ed7bfcb151a03f3
Asim Mahmood <31977970+asimmahmood88@users.noreply.github.com>
2021-06-04 12:02:58 +0500

    Updated ChatFeed.jsx
    Rendered messages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+
 import  ChatFeed  from './components/ChatFeed';
 
+
+
 const App = () => {
-    return <ChatEngine 
+    return (
+    <ChatEngine 
         height="100vh"
-        projectID="c2ba0d03-7dd6-4200-8099-40f95a10c0ab"
-        userName="mecallmathers"
-        userSecret="123123"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-    />   
-
-}
-
+        projectID=""
+        userName=""
+        userSecret=""
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+        />
+    );
+      
+    
+}  
+    
 export default App;
--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,38 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
 const ChatFeed = (props) => {
-    console.log(props);
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const LastMessageKey = index === 0 ? null : keys[index - 1];
+            const IsMyMessage = userName === message.sender.userName;
+
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
+                    <div className="message-block">
+                        {
+                            IsMyMessage
+                            ? <MyMessage />
+                            :<TheirMessage />
+                        }
+
+                    </div>
+                        <div className="read-receipts" style={{ marginRight: isMymessage ? '18px' : '0px', marginLeft: isMymessage ? '0px' : '68px'}}>
+                            read-receipts
+
+
+                        </div>
+                </div>
+            )
+        })
+    }
     return(
         <div>
             ChatFeed
